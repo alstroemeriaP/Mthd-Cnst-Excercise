bcfd489aa9bcfb6be07403cb07ba7ecd7f0e257c
Feysha Rahadian Syah <101683544+alstroemeriaP@users.noreply.github.com>
2022-04-08 20:43:37 +0700

    latihanMethod1

---
+++ pemrogramanlatihan/Calculator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pemrogramanlatihan
+{
+    class Calculator
+    {
+        public static int penjumlah(int a, int b)
+        {
+            return a + b;
+        }
+        public static int penjumlah (int a, int b, int c)
+        { 
+            return a + b + c;
+        }
+        public static int pengurangan (int a, int b)
+        {
+            return a - b;
+        }
+        public static int perkalian(int a, int b)
+        {
+            return a * b;
+        }
+    }
+}
