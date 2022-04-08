22048a3ffa6b5e7292ccb2eaaa0b72277648a46a
Feysha Rahadian Syah <101683544+alstroemeriaP@users.noreply.github.com>
2022-04-08 21:06:47 +0700

    -

---
+++ TugasMethodTest/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace TugasMethodTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 3, 9, 5 };
+            int[] numbers2 = { 2, 6, 5 };
+            int biggestNumber = numbers.Max();
+            int biggestNumber2 = numbers2.Max();
+            int smallestNumber = numbers.Min();
+            int smallestNumber2 = numbers2.Min();
+
+            Console.WriteLine("Maximum #1: {0}", biggestNumber);
+            Console.WriteLine("Maximum #2: {0}", biggestNumber2);
+            Console.WriteLine();
+            Console.WriteLine("Minimum #1: {0}", smallestNumber);
+            Console.WriteLine("Minimum #2: {0}", smallestNumber2);
+
+            Console.ReadKey();
+        }
+    }
+}
