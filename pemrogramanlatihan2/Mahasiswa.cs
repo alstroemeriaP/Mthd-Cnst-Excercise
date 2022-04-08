600c7b428ecfe8d49deb5610d93ce36f6828942c
Feysha Rahadian Syah <101683544+alstroemeriaP@users.noreply.github.com>
2022-04-08 20:46:07 +0700

    Add files via upload

---
+++ pemrogramanlatihan2/Mahasiswa.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pemrogramanlatihan2
+{
+    public class Mahasiswa
+    {
+        public Mahasiswa()
+        {
+
+        }
+        public Mahasiswa(string nim, string nama)
+        {
+            Nim = nim;
+            Nama = nama;
+        }
+        public string Nim { get; set; } = default!;
+        public string Nama { get; set; } = default!;
+    }
+}
